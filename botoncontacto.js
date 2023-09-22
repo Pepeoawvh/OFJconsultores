e3978a51bf2e47c61a14a3c399d724dfe656f3c7
unknown <jpp.valdes@gmail.com>
2023-09-21 23:38:42 -0300

    navbar en contacto, formulario en inicio

---
+++ botoncontacto.js
@@ -0,0 +1,8 @@
+const menunav=document.getElementById("menunav")
+const menuhide=document.getElementById("menuhide")
+
+function togglemenu() {
+    menuhide.classList.toggle('oculto')
+}
+
+menunav.addEventListener("click",togglemenu);
